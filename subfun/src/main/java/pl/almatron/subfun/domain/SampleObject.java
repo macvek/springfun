a8a589e6138a9d029faabcae9f8b0c09def60d72
Maciej Aleksandrowicz <macvek@gmail.com>
2014-11-19 13:53:56 +0100

    ADDED annotated entity to hibernate !!! and made it working :)

--- subfun/src/main/java/pl/almatron/subfun/domain/SampleObject.java
+++ subfun/src/main/java/pl/almatron/subfun/domain/SampleObject.java
@@ -3,12 +3,22 @@ package pl.almatron.subfun.domain;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 /**
  * subfun
  *
  * @author macvek
  */
+@Entity
+@Table(name="sampleObjects")
 public class SampleObject {
     private Long id;
     private String title;
@@ -16,6 +26,8 @@ public class SampleObject {
 
     private Set<SampleItem> items = new HashSet<>();
     
+    @Id
+    @GeneratedValue
     public Long getId() {
         return id;
     }
@@ -32,6 +44,7 @@ public class SampleObject {
         this.title = title;
     }
 
+    @Column(name="sampletimestamp")
     public Date getDate() {
         return date;
     }
@@ -40,6 +53,14 @@ public class SampleObject {
         this.date = date;
     }
 
+    @ManyToMany(
+        targetEntity=SampleItem.class
+    )
+    @JoinTable(
+        name="sampleItems_sampleObjects",
+        joinColumns=@JoinColumn(name="object_id"),
+        inverseJoinColumns=@JoinColumn(name="item_id")
+    )
     public Set<SampleItem> getItems() {
         return items;
     }
