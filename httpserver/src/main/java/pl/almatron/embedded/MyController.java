8d037efe404179bb41d771c88bf3520ef40b84a3
Maciej Aleksandrowicz <macvek@gmail.com>
2014-11-20 20:09:17 +0100

    Added killserver with another command and rendering to html

--- httpserver/src/main/java/pl/almatron/embedded/MyController.java
+++ httpserver/src/main/java/pl/almatron/embedded/MyController.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import static org.springframework.web.context.WebApplicationContext.SCOPE_SESSION;
 
 /**
@@ -21,14 +22,21 @@ public class MyController {
     
     private int timesToKill = 3;
     
-    @RequestMapping(method = RequestMethod.GET)
-    public String getIt() {
+    @RequestMapping(value="killserver")
+    @ResponseBody
+    public String killServer() {
         if (--timesToKill == 0) {
             killServer.kill();
             return "Server killed";
         }
         else {
-            return "Left_times_"+timesToKill;
+            return "<h1>times left "+timesToKill+"</h1><br/><a href=\"killserver\">Try again!</a>";
         }
+    }
+    
+    @RequestMapping("/")
+    @ResponseBody
+    public String actionWelcome() {
+        return "<h1>welcome</h1><br/><a href=\"killserver\">Server killer</a>";
     }
 }
