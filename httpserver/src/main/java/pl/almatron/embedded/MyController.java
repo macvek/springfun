f5ef99cd4d280c12dc34f451e688a76e87610ff3
Maciej Aleksandrowicz <macvek@gmail.com>
2014-11-22 11:48:23 +0100

    cleanup

--- httpserver/src/main/java/pl/almatron/embedded/MyController.java
+++ httpserver/src/main/java/pl/almatron/embedded/MyController.java
@@ -1,12 +1,9 @@
 package pl.almatron.embedded;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import static org.springframework.web.context.WebApplicationContext.SCOPE_SESSION;
 
 /**
  * httpserver
